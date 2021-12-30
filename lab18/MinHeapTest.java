97593fab43d6c6cb09fbd2f999f5780957a6c834
xyhuang1116 <xyhuang1116@gmail.com>
2021-12-30 17:59:05 +0100

    finished lab18

--- lab18/MinHeapTest.java
+++ lab18/MinHeapTest.java
@@ -5,5 +5,20 @@ public class MinHeapTest {
 
     @Test
     public void test1() {
+        MinHeap<Character> mhpq = new MinHeap<>();
+        mhpq.insert('f');
+        assertEquals(1,mhpq.size());
+        assertEquals("f", mhpq.findMin().toString());
+        assertTrue(mhpq.contains('f'));
+        mhpq.insert('h');
+        mhpq.insert('d');
+        mhpq.insert('b');
+        mhpq.insert('c');
+        assertEquals(5,mhpq.size());
+        mhpq.removeMin();
+        mhpq.removeMin();
+        assertEquals("d", mhpq.findMin().toString());
+        assertEquals(3,mhpq.size());
+        assertFalse(mhpq.contains('c'));
     }
 }
